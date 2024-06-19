871d03cd0192b3a322e706ddf62949cc6e78981f
zhangqi <zhangqixcu@gmail.com>
2024-06-19 17:34:30 +0800

    update: upload TF, develop account

--- SwiftUIDemo/ZStackViewDemo.swift
+++ SwiftUIDemo/ZStackViewDemo.swift
@@ -9,16 +9,50 @@ import SwiftUI
 
 struct ZStackViewDemo: View {
     
-    
+    @State var count = 0;
     
     var body: some View {
-        Text("文本展示")
-        ZStack {
-            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-                .offset(x: 20,y: 20)
-                .font(.system(size: 13))
-                .padding()
+        NavigationView {
+            List {
+                NavigationLink(destination: Text("Detail View 1")) {
+                    HStack(alignment: .center, content: {
+                        Button(action: {
+                            count += 1
+                        }, label: {
+                            Text("点我 + 1")
+                                .padding()
+                                .background(Color(.tertiarySystemFill))
+                                .cornerRadius(5)
+                        })
+                        
+                        Button(action: {
+                            count -= 1
+                        }, label: {
+                            Text("点我 - 1")
+                                .padding()
+                                .background(Color(.tertiarySystemFill))
+                                .cornerRadius(5)
+                        })
+                    })
+                    .background(Color(.orange.withAlphaComponent(0.2)))
+                    .cornerRadius(8.0)
+                }
+                NavigationLink(destination: Text("Detail View 2")) {
+                    HStack(alignment: .top, content: {
+                        Image("empty")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 50, height: 50)
+                        
+                        Text("当前值：\(count)")
+                            .font(.system(size: 18, weight: .bold))
+                    })
+                    .padding(EdgeInsets(top: 15, leading: 0, bottom: 20, trailing: 0))
+                }
+            }
+            .navigationTitle("Navigation Example")
         }
+        
     }
 }
 
